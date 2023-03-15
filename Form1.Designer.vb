dd72717871166c01381d19c9eee25d60423d2e1d
arrionneh <123421615+arrionneh@users.noreply.github.com>
2023-03-15 04:03:18 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.btnSpeed = New System.Windows.Forms.Button()
+        Me.lstSpeed = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblAvg = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.BackColor = System.Drawing.Color.White
+        Me.lblHeading.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblHeading.Font = New System.Drawing.Font("Gill Sans Ultra Bold Condensed", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.Black
+        Me.lblHeading.Location = New System.Drawing.Point(369, 41)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(450, 49)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Internet Speed Test Survey"
+        '
+        'btnSpeed
+        '
+        Me.btnSpeed.BackColor = System.Drawing.Color.SandyBrown
+        Me.btnSpeed.Font = New System.Drawing.Font("Gill Sans Ultra Bold Condensed", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpeed.Location = New System.Drawing.Point(425, 155)
+        Me.btnSpeed.Name = "btnSpeed"
+        Me.btnSpeed.Size = New System.Drawing.Size(262, 56)
+        Me.btnSpeed.TabIndex = 1
+        Me.btnSpeed.Text = "Enter Internet Speed"
+        Me.btnSpeed.UseVisualStyleBackColor = False
+        '
+        'lstSpeed
+        '
+        Me.lstSpeed.FormattingEnabled = True
+        Me.lstSpeed.ItemHeight = 20
+        Me.lstSpeed.Location = New System.Drawing.Point(792, 131)
+        Me.lstSpeed.Name = "lstSpeed"
+        Me.lstSpeed.ScrollAlwaysVisible = True
+        Me.lstSpeed.Size = New System.Drawing.Size(120, 304)
+        Me.lstSpeed.TabIndex = 2
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.SandyBrown
+        Me.btnClear.Font = New System.Drawing.Font("Gill Sans Ultra Bold Condensed", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(582, 574)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(128, 38)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lblAvg
+        '
+        Me.lblAvg.AutoSize = True
+        Me.lblAvg.BackColor = System.Drawing.Color.White
+        Me.lblAvg.Font = New System.Drawing.Font("Gill Sans Ultra Bold Condensed", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAvg.Location = New System.Drawing.Point(606, 499)
+        Me.lblAvg.Name = "lblAvg"
+        Me.lblAvg.Size = New System.Drawing.Size(0, 30)
+        Me.lblAvg.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.weekly6.My.Resources.Resources.speed
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1027, 638)
+        Me.Controls.Add(Me.lblAvg)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstSpeed)
+        Me.Controls.Add(Me.btnSpeed)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "Form1"
+        Me.Text = "Internet Speed Test Survey"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents btnSpeed As Button
+    Friend WithEvents lstSpeed As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblAvg As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+    Private Sub btnSpeed_Click(sender As Object, e As EventArgs) Handles btnSpeed.Click
+        Dim strSpeed As String
+        Dim decSpeed As Decimal
+        Dim decSumofSpeeds As Decimal
+        Dim decAverage As Decimal = 0D
+
+        Dim strIBoxMsg As String = "Enter the # of Mbps of your home Internet speed user #"
+        Dim strIBoxTitle As String = "Internet Speed"
+        Dim strIBoxNumericErr As String = "Error - Enter the speed of your home Internet speed user #"
+        Dim strErrMsg As String = "Error - Enter valid speed"
+
+        Dim intMaxEntries As Integer = 10
+        Dim intEntries As Integer = 1
+
+        strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+
+        Do Until intEntries > intMaxEntries Or strSpeed = ""
+            If IsNumeric(strSpeed) Then
+                decSpeed = Convert.ToDecimal(strSpeed)
+                If decSpeed > 0 Then
+                    lstSpeed.Items.Add(decSpeed)
+                    decSumofSpeeds += decSpeed
+                    intEntries += 1
+                    strIBoxMsg = strIBoxMsg
+                Else
+                    strIBoxMsg = strErrMsg
+                End If
+            Else
+                strIBoxMsg = strIBoxNumericErr
+            End If
+            If intEntries <= intMaxEntries Then
+                strSpeed = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+            End If
+        Loop
+
+        lblAvg.Visible = True
+        If intEntries > 1 Then
+            decAverage = decSumofSpeeds / (intEntries - 1)
+            lblAvg.Text = "Average Internet Speed: " & decAverage.ToString("F2") & " Mbps "
+        Else
+            lblAvg.Text = "No Speed Values Entered"
+        End If
+
+        btnSpeed.Enabled = False
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstSpeed.Items.Clear()
+        lblAvg.Text = " "
+        btnSpeed.Enabled = True
+    End Sub
+End Class
